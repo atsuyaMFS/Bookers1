0d80a935b06b69c0bde0c875e26f35f7dc10739b
atsuyaMFS <ec2-user@ip-172-31-19-78.ec2.internal>
2021-07-28 09:14:22 +0000

    first commit

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,56 @@
+class BooksController < ApplicationController
+  def index
+    @books = Book.all
+    @book = Book.new(params[:id])
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if  @book.update(book_params)
+      flash[:success] = "Book was successfully updated."
+      redirect_to book_path(@book)
+    else
+      render :edit
+    end
+
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if  @book.save
+      flash[:success] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    if  book.destroy
+      flash[:success] = "Book was successfully destroyed."
+      redirect_to books_path
+    else
+      flash[:failed] ="Book wasn't destroied"
+      render :index
+    end
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title,:body)
+  end
+end
